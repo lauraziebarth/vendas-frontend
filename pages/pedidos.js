607b0451f9b6474c6f3e7b82d999fa1d92666bfd
Laura Ziebarth <lauragzie@gmail.com>
2021-05-21 19:53:50 -0300

    refatora pagina de pedidos

--- pages/pedidos.js
+++ pages/pedidos.js
@@ -1,90 +1,22 @@
-function Pedidos() {
+import Layout from "../layouts/admin/layout"
+import Header from "../components/header"
+import Button from "../components/button"
+import TableOrders from "../components/table-orders"
+
+function Orders() {
+    const orders = [{id:1, numero:"1", cliente_nome: "Laura", total:59.00, rentabilidade:"boa"},
+                    {id:2, numero:"2", cliente_nome: "Murillo", total:69.00, rentabilidade:"ruim"}, 
+                    {id:3, numero:"3", cliente_nome: "Alice", total:79.00, rentabilidade:"normal"}]
+
     return (
-        <main>
+            <Layout>
+                <Header title="Pedidos">
+                    <Button title="Criar Pedido"/>
+                </Header>
 
-            <div className="page-box">
-                <nav className="menu-lateral">
-                    <li className="menu-lateral-pedido">
-                        <a href="/pedidos">Pedidos</a>
-                    </li>
-                    <li className="menu-lateral-produtos">
-                        <a href="/produtos">Produtos</a>
-                    </li>
-                    <li className="menu-lateral-clientes">
-                        <a href="/clientes">Clientes</a>
-                    </li>
-                    <li className="menu-lateral-usuarios">
-                        <a href="/usuarios">Usuários</a>
-                    </li>
-                </nav>
-
-                <div className="pagina-pedidos">
-                    <div className="titulo-principal">
-                        <h1>Pedidos</h1>
-                    </div>
-                    <div className="box-button-criar-pedido">
-                        <button type="button" className="button-criar-pedido">+ Criar Pedido</button>
-                    </div>
-                </div>
-            </div>
-        
-            <style jsx>
-                {`
-                .page-box{
-                    display: flex;
-                    flex-direction: row;
-                    background-color: #ffffff;
-                    max-width: 1300px;
-                    margin: 200px auto;
-                    border-radius: 3px;
-                }
-                .menu-lateral{
-                    display: flex;
-                    flex-direction: column;
-                    list-style: none;
-                    border-right: 2px solid #f2f2f2;
-                    width: 10%;                 
-                }
-                .menu-lateral li{
-                    list-style: none;
-                    margin: 30px;
-                }
-                .menu-lateral a{
-                    text-decoration: none;
-                }
-                .pagina-pedidos{
-                    display: flex;
-                    flex-direction: column;
-                    width: 80%;
-                }
-                .titulo-principal{
-                    display: flex;
-                    flex-direction: column;
-                    align-content: top;
-                    margin-left: 50px;
-                    border-bottom: 2px solid #f2f2f2;
-                }
-                .box-button-criar-pedido{
-                    display: flex;
-                    align-content: top;
-                    margin-top: 30px;
-                    margin-left: 50px;
-                }
-                .button-criar-pedido{
-                    padding: 10px;
-                    border: 1px solid #f2f2f2;
-                    border-radius: 10px;
-                    width: auto;
-                    background-color: #4B2F7E;
-                    color: #ffffff;
-                    justify-content: flex-end;
-                    font-weight: bolder;
-                }
-                `}
-            </style>
-            
-        </main>
-      ) 
+                <TableOrders list={orders}/>
+            </Layout>
+    ) 
   }
   
-  export default Pedidos
+  export default Orders
