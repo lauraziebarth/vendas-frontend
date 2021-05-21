3ca09726b5bab4287d6992f0a3c4dee58c127b84
Laura Ziebarth <lauragzie@gmail.com>
2021-05-21 19:35:36 -0300

    Cria componente button

---
+++ components/button.js
@@ -0,0 +1,30 @@
+export default function Button({titulo}){
+    return(
+        <div className="box-button">
+            <button type="button" className="button">+ {titulo}</button>
+        
+        <style jsx>
+            {`
+            .box-button{
+                display: flex;
+                align-content: top;
+                margin-top: 30px;
+                margin-left: 50px;
+            }
+            .button{
+                padding: 10px;
+                border: 1px solid #f2f2f2;
+                border-radius: 10px;
+                width: auto;
+                background-color: #4B2F7E;
+                color: #ffffff;
+                justify-content: flex-end;
+                font-weight: bolder;
+            }
+            `}
+        </style>
+
+        </div>
+    )
+}
+
