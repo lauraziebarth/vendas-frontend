f592e78c193c39c90f2aafb0031d8901e84bd704
Laura Ziebarth <lauragzie@gmail.com>
2021-05-21 19:47:44 -0300

    Cria componente de rentabilidade

---
+++ components/rentability.js
@@ -0,0 +1,22 @@
+export default function Rentability({titulo}){
+    return(
+        <div className="box-rentability-order">
+            <span className="rentability">o</span>
+        
+        <style jsx>
+            {`
+            .rentability{
+                font-size: 13px;
+                color: #00872d;
+                background-color: #00872d;
+                border-radius: 100px;
+                text-align: center;
+                padding: 1px 5px;
+            }
+            `}
+        </style>
+
+        </div>
+    )
+}
+
