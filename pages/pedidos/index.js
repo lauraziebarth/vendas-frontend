56a95bce9630c0f761930c91772971a54f428500
Laura Ziebarth <lauragzie@gmail.com>
2021-06-07 21:22:27 -0300

    ordena lista de pedidos

---
+++ pages/pedidos/index.js
@@ -0,0 +1,38 @@
+import Layout from "../../layouts/admin/layout"
+import Header from "../../components/header"
+import Button from "../../components/button"
+import TableOrders from "../../components/table-orders"
+
+import { useRouter } from 'next/router'
+
+// This gets called on every request
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(`http://127.0.0.1:8000/pedidos/`)
+    const data = await res.json()
+    const dataDesc = data.sort((a , b) => b.id - a.id)
+    // Pass data to the page via props
+    return { props: { data: dataDesc } }
+}
+
+function Orders({data=[]}) {
+
+    const router = useRouter()
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        router.push("/pedidos/pedido")
+      }
+
+    return (
+            <Layout>
+                <Header title="Pedidos">
+                    <Button title="Criar Pedido" onClick={handleClick}/>
+                </Header>
+
+                <TableOrders list={data}/>
+            </Layout>
+    ) 
+  }
+  
+  export default Orders
